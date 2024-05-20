f16f851bb1a94352b49cd1e1a8bcbb63a60ca7c9
Reptilian6163 <112165840+Reptilian6163@users.noreply.github.com>
2024-05-20 10:12:26 +0000

    fetch_response.js and changes to other files

--- src/lib/post_request.js
+++ src/lib/post_request.js
@@ -2,22 +2,24 @@ const OpenAI = require("openai");
 const dotenv = require("dotenv");
 dotenv.config();
 
-// Gets API key
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
 });
 
-// Current messages are temporary placeholders
-async function main() {
-    const completion = await openai.chat.completions.create({
-        messages: [{ role: "system", content: "You are a helpful assistant." },
-            {role: "user", content: "Tell me about APIs in a short paragraph."}],
-        model: "gpt-4o",
-    });
+async function main(user_message) {
+    try {
+        const completion = await openai.chat.completions.create({
+            messages: [{role: "system", content: "You are a helpful assistant."},
+                {role: "user", content: user_message}], // make content the user input
+            model: "gpt-3.5-turbo",
+        });
+        console.log(completion.choices[0].message.content);
+        return (completion.choices[0].message.content);
 
-    console.log(completion.choices[0].message.content);
+    } catch (error) {
+        console.error("There was an error calling the API: ", error);
+        throw error;
+    }
 }
 
-// Function export
 module.exports = main;
-
