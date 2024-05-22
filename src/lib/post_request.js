b1cdf99126e0bcbd91f9c8a2ee8fe38bf4c4966f
Reptilian6163 <112165840+Reptilian6163@users.noreply.github.com>
2024-05-22 12:16:48 +0000

    history has been added

--- src/lib/post_request.js
+++ src/lib/post_request.js
@@ -1,5 +1,6 @@
 const OpenAI = require("openai");
 const dotenv = require("dotenv");
+const history = require('./chat_history');
 dotenv.config();
 
 const openai = new OpenAI({
@@ -10,9 +11,14 @@ async function main(user_message) {
     try {
         const completion = await openai.chat.completions.create({
             messages: [{role: "system", content: "You are a helpful assistant."},
+                ...history.retrieve_history(),
                 {role: "user", content: user_message}], // make content the user input
-            model: "gpt-3.5-turbo",
+            model: "gpt-4o",
         });
+        
+        // Adds user input and API response to history. //
+        history.add_to_history(user_message, completion.choices[0].message.content);
+
         console.log(completion.choices[0].message.content);
         return (completion.choices[0].message.content);
 
